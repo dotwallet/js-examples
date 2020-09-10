c6b9a0af9b3522d5a2aba67223828c6170b393f9
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-09-10 16:42:28 +0800

    app id

--- dice-game/client/src/config.js
+++ dice-game/client/src/config.js
@@ -1,9 +1,9 @@
 export const SERVER_URL =
   process.env.NODE_ENV == 'production'
     ? 'https://bitcoin-fair-dice.herokuapp.com'
-    : 'http://192.168.1.142:3001';
+    : 'http://192.168.1.112:3001';
 export const CLIENT_URL =
   process.env.NODE_ENV == 'production'
     ? 'https://musing-pike-d80e67.netlify.app'
-    : 'http://192.168.1.142:8080';
-export const APP_ID = '89d001043806644fdb4fb14099ff6be5';
+    : 'http://192.168.1.112:8080';
+export const APP_ID = 'c260a14012c883e7ec4be8e15af86f95';
