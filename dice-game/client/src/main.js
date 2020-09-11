8cdb9a0c13402e5032669202b4de2f299cb1f425
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-09-11 17:16:49 +0800

    in component?

--- dice-game/client/src/main.js
+++ dice-game/client/src/main.js
@@ -4,7 +4,6 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import './assets/style/index.css';
-import './assets/style/slider.css';
 
 import VueConfetti from 'vue-confetti';
 
