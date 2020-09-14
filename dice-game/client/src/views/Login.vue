2c33133c7c2e6569dfa029f888c998b19deaa5b8
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-09-14 16:15:33 +0800

    login size

--- dice-game/client/src/views/Login.vue
+++ dice-game/client/src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="scroller flex flex-col items-center min-w-full bg-primary">
-    <img class="max-w-md" alt="App Logo" src="../assets/logo-screen.png" />
+    <img class="max-w-xs sm:max-w-md" alt="App Logo" src="../assets/logo-screen.png" />
     <dotwallet-login
       :app-id="APP_ID"
       :redirect-url="SERVER_URL + '/auth'"
