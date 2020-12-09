6093f18036ba1659665cf14c1f1abb1be5bce805
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-12-09 16:03:14 +0800

    接口路径

--- simple-store/src/config.js
+++ simple-store/src/config.js
@@ -1,4 +1,4 @@
 const CLIENT_ID = `89d001043806644fdb4fb14099ff6be5`;
 const APP_URL = window.location.origin;
-const DOTWALLET_API = `https://api.ddpurse.com/v1`; // https://staging.api.ddpurse.com/v1
+const DOTWALLET_API = `https://api.ddpurse.com`; // https://staging.api.ddpurse.com //note: no /v1/
 const DOTWALLET_CLIENT = 'https://ddpurse.com'; // https://prerelease.ddpurse.com
